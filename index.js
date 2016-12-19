db8f362bf0bb344119e51e6169356968a42d681c
IntPtr <lxy.lixiaoyan@gmail.com>
2016-12-19 21:02:11 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: "airbnb",
+  env: {
+    browser: true,
+  },
+  rules: {
+    quotes: ["error", "double"],
+    "no-plusplus": "off",
+    "no-implicit-coercion": ["error", { boolean: true }],
+    "no-invalid-this": "error",
+    "no-param-reassign": ["error", { props: false }],
+    "no-else-return": "off",
+    "no-cond-assign": ["error", "except-parens"],
+    "no-unmodified-loop-condition": "error",
+    "no-implicit-globals": "error",
+    camelcase: ["error", { properties: "always" }],
+    "prefer-reflect": "error",
+    "react/jsx-filename-extension": ["error", { extensions: [".js"] }],
+    "react/self-closing-comp": ["error", { html: false }],
+    "react/prefer-stateless-function": "off",
+    "react/forbid-prop-types": "off",
+  },
+};
