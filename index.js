a909c8615bb3f5c54bee9f75372d3f483148d660
IntPtr <lxy.lixiaoyan@gmail.com>
2017-01-15 01:56:45 +0800

    update rules

--- index.js
+++ index.js
@@ -20,7 +20,6 @@ module.exports = {
     camelcase: ["error", { properties: "always" }],
     "prefer-reflect": "error",
     "react/jsx-filename-extension": ["error", { extensions: [".js"] }],
-    "react/self-closing-comp": ["error", { html: false }],
     "react/prefer-stateless-function": "off",
     "react/forbid-prop-types": "off",
   },
